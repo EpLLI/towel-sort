1a1e44e22f054dadcbece7b7affb29dda8ce8686
EpLLI <staniush.iv@gmail.com>
2020-10-25 16:42:39 +0300

    feat: add functional

--- src/index.js
+++ src/index.js
@@ -2,5 +2,29 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
+  let m=matrix;
+  let arr = [];
+  let c=0;
+  if (matrix===undefined)
+  {return arr;}
+  else {
+  for(var i=0; i<matrix.length; i++){ 
+    if (i%2===0)  {
+    for(var j=0; j<matrix[i].length; j++){
+      arr[c] = matrix[i][j];
+      c++;
+    }
+    }
+    if(i%2===1)
+    {
+      let z=matrix[i].length-1;
+      for(var j=0; j<matrix[i].length; j++){
+        arr[c] = matrix[i][z];
+        c++;
+        z--;
+      }
+      }
+    }
+  return arr;}
 }
+
